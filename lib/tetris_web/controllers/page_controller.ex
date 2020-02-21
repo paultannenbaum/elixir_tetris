c7cc646e7f0a69c7816243ebcf7b9bb2639fb0a2
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-02-21 15:41:29 -0800

    Refactor creation logic into context

--- lib/tetris_web/controllers/page_controller.ex
+++ lib/tetris_web/controllers/page_controller.ex
@@ -1,11 +1,14 @@
 defmodule TetrisWeb.PageController do
   use TetrisWeb, :controller
 
+  alias Tetris.Board
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def game(conn, _params) do
-    render(conn, "board.html")
+    board = Board.new_board()
+    render(conn, "game.html", board: board)
   end
 end
--- lib/tetris_web/views/page_view.ex
+++ lib/tetris_web/views/page_view.ex
@@ -1,16 +1,11 @@
 defmodule TetrisWeb.PageView do
   use TetrisWeb, :view
 
-  @x_cells 40
-  @y_cells 80
-
-  def create_board_cells(num_wide \\ @x_cells, num_tall \\ @y_cells) do
-    cell_list = for y <- num_tall..0,
-                    x <- 0..num_wide,
-                    do: {x,y}
-
-    cell_list
-    |> Enum.map(fn {x, y} -> content_tag :span, "", [{:data, [x: x]}, {:data, [y: y]}, class: "cell"] end)
-    |> html_escape
+  # translates board object into something the DOM can understand
+  def board_as_html(board) do
+    board.cells
+    |> Enum.sort(&(&1.y >= &2.y))
+    |> Enum.map(fn %{x: x, y: y, color: color} ->
+      content_tag :span, "", [{:data, [x: x]}, {:data, [y: y]}, class: "cell #{color}"] end)
   end
 end
