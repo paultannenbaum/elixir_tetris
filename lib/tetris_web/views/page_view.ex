09ac3677214d607a728131cd3e0baa21fa7b4577
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-02-21 14:22:03 -0800

    Renders a board

--- lib/tetris_web/views/page_view.ex
+++ lib/tetris_web/views/page_view.ex
@@ -1,16 +1,16 @@
 defmodule TetrisWeb.PageView do
   use TetrisWeb, :view
 
-  @num_x_cells 40
-  @num_y_cells 80
+  @x_cells 40
+  @y_cells 80
 
-  def create_board_cells do
-    cell_list = for x <- 0..@num_x_cells,
-                    y <- 0..@num_y_cells,
+  def create_board_cells(num_wide \\ @x_cells, num_tall \\ @y_cells) do
+    cell_list = for y <- num_tall..0,
+                    x <- 0..num_wide,
                     do: {x,y}
 
     cell_list
-    |> Enum.map(fn {x, y} -> content_tag :span, "x: #{x}, y: #{y}" end)
+    |> Enum.map(fn {x, y} -> content_tag :span, "", [{:data, [x: x]}, {:data, [y: y]}, class: "cell"] end)
     |> html_escape
   end
 end
