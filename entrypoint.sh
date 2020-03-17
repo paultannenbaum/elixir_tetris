166af9e7cbcd669499dff68a9489dc7edacb4b2c
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-03-16 17:13:14 -0700

    Add necesary configurations

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+# File: docker_phx/entrypoint.sh
+#!/bin/bash
+# docker entrypoint script.
+
+# assign a default for the database_user
+DB_USER=${DATABASE_USER:-postgres}
+
+# wait until Postgres is ready
+while ! pg_isready -q -h $DATABASE_HOST -p 5432 -U $DB_USER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+bin="/app/bin/tetris"
+eval "$bin eval \"Tetris.Release.migrate\""
+# start the elixir application
+exec "$bin" "start"
